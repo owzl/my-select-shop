778167b2adeebffa649dc5b6110de4c5fc8d98c4
owzl <alliwannado1211@gmail.com>
2025-09-25 15:40:10 +0900

    1

---
+++ myselectshop/src/main/java/com/sparta/myselectshop/naver/dto/ItemDto.java
@@ -0,0 +1,20 @@
+package com.sparta.myselectshop.naver.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ItemDto {
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+
+    public ItemDto(JSONObject itemJson) {
+        this.title = itemJson.getString("title");
+        this.link = itemJson.getString("link");
+        this.image = itemJson.getString("image");
+        this.lprice = itemJson.getInt("lprice");
+    }
+}
